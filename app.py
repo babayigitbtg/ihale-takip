a0cb373e8c55905f9bbcb93df42065a1b74d098d
babayigitbtg <babayigitbtg@gmail.com>
2026-06-17 09:57:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://ihaleciler.com/tenders/search?workcategory_in=37&workcategory_in=39&workcategory_in=505&workcategory_in=507&province_in=Aksaray&province_in=Karaman&province_in=Konya&province_in=Nev%C5%9Fehir&tender_ts_meta=&buyermeta_ts=&contentlast_gte=&contentlast_lte=&offerend_gte=&offerend_lte=&workterm_gte=&workterm_lte=&workdelivery_gte=&workdelivery_lte=&workpayment_gte=&workpayment_lte=&display=advanced"
+url = "url = "https://www.konya.bel.tr/ihale""
 
 r = requests.get(url, timeout=30)
 
