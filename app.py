3dd93e025ac76eddc7db4e5439e7d45b6f437a54
babayigitbtg <babayigitbtg@gmail.com>
2026-06-17 09:24:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Merhaba, ihale takip sistemi çalışıyor")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+requests.get(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    params={
+        "chat_id": CHAT_ID,
+        "text": "✅ GitHub Actions üzerinden ilk test mesajı geldi."
+    }
+)
+
+print("Mesaj gönderildi")
