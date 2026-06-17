855c84fad19e6869ed725a3e471fcaa2563f317a
babayigitbtg <babayigitbtg@gmail.com>
2026-06-17 09:54:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-import os
 import requests
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
+url = "https://ihaleciler.com/tenders/search?workcategory_in=37&workcategory_in=39&workcategory_in=505&workcategory_in=507&province_in=Aksaray&province_in=Karaman&province_in=Konya&province_in=Nev%C5%9Fehir&tender_ts_meta=&buyermeta_ts=&contentlast_gte=&contentlast_lte=&offerend_gte=&offerend_lte=&workterm_gte=&workterm_lte=&workdelivery_gte=&workdelivery_lte=&workpayment_gte=&workpayment_lte=&display=advanced"
 
-requests.get(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    params={
-        "chat_id": CHAT_ID,
-        "text": "✅ GitHub Actions üzerinden ilk test mesajı geldi."
-    }
-)
+r = requests.get(url, timeout=30)
 
-print("Mesaj gönderildi")
+print("STATUS:", r.status_code)
+print(r.text[:2000])
