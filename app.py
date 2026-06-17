91286a86b3a7767f4540c28eb7958c79dedc110d
babayigitbtg <babayigitbtg@gmail.com>
2026-06-17 10:02:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://www.konya.bel.tr/ihale"
 
 r = requests.get(url, timeout=30)
 
 print("STATUS:", r.status_code)
-print(r.text[:2000])
+
+soup = BeautifulSoup(r.text, "html.parser")
+
+for a in soup.find_all("a"):
+    text = a.get_text(strip=True)
+
+    if len(text) > 20:
+        print(text)
