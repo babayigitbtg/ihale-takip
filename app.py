1f91dfed4e6988110ff51d3376325cfffec2d6df
babayigitbtg <babayigitbtg@gmail.com>
2026-06-17 10:07:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ url = "https://www.konya.bel.tr/ihale"
 r = requests.get(url, timeout=30)
 soup = BeautifulSoup(r.text, "html.parser")
 
-cards = soup.find_all(["div", "article", "section"])
+for tag in soup.find_all(True):
+    text = tag.get_text(" ", strip=True)
 
-for i, card in enumerate(cards[:100]):
-    text = card.get_text(" ", strip=True)
-
-    if "İhale Konusu" in text:
-        print("\n" + "="*50)
-        print("TAG:", card.name)
-        print("CLASS:", card.get("class"))
-        print(text[:500])
+    if "Karahüyük Mahallesi" in text:
+        print("=" * 50)
+        print("TAG:", tag.name)
+        print("CLASS:", tag.get("class"))
+        print("ID:", tag.get("id"))
+        print(text[:300])
+        break
