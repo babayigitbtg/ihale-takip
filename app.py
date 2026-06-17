e0e8f70c1eafe2faf06aeebf9b57e207a85c4166
babayigitbtg <babayigitbtg@gmail.com>
2026-06-17 10:12:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import requests
-from bs4 import BeautifulSoup
 
-url = "https://www.konya.bel.tr/ihale"
+urls = [
+    "https://www.konya.gov.tr/ihale-ilanlari",
+    "https://www.kos.org.tr/directorate/auctions",
+    "https://www.ilan.gov.tr/ilan/kategori/9/ihale-duyurulari?aci=62&txv=9&field=publish_time&order=desc",
+    "https://ekapv2.kik.gov.tr/ekap/search"
+]
 
-r = requests.get(url, timeout=30)
-soup = BeautifulSoup(r.text, "html.parser")
-
-for tag in soup.find_all(True):
-    text = tag.get_text(" ", strip=True)
-
-    if "Karahüyük Mahallesi" in text:
-        print("=" * 50)
-        print("TAG:", tag.name)
-        print("CLASS:", tag.get("class"))
-        print("ID:", tag.get("id"))
-        print(text[:300])
-        break
+for url in urls:
+    try:
+        r = requests.get(url, timeout=30)
+        print(url)
+        print("STATUS:", r.status_code)
+        print("-" * 50)
+    except Exception as e:
+        print(url)
+        print("HATA:", e)
+        print("-" * 50)
