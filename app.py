e7332fc2bf7bfae3d06760ba9a8eba007fffa9dc
babayigitbtg <babayigitbtg@gmail.com>
2026-06-17 17:32:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.kos.org.tr/directorate/auctions"
+url = "https://www.konya.bel.tr/ihale"
 
 r = requests.get(url, timeout=30)
+
 soup = BeautifulSoup(r.text, "html.parser")
 
-for a in soup.find_all("a", href=True):
-    text = a.get_text(" ", strip=True)
+for h in soup.find_all(["h1", "h2", "h3", "h4", "h5", "a"]):
+    text = h.get_text(" ", strip=True)
 
-    if len(text) > 10:
+    if len(text) > 20:
         print(text)
